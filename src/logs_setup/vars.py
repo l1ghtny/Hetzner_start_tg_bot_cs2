3a4070d83b28d46c9525854342a2e557a1af946f
lightny <ankoshenkov@gmail.com>
2024-02-02 20:01:22 +0300

    debug of logs for docker

--- src/logs_setup/vars.py
+++ src/logs_setup/vars.py
@@ -6,8 +6,10 @@ load_dotenv()
 windows_dir = os.getenv("path_main")
 
 target_dir = "logs"
+print(f'target: {target_dir}')
 if platform.system() == "Linux":
     root_dir = "home/tgserver"
+    print(f'root: {root_dir}')
 else:
     disk = "F:\\"
     dir = f"""{windows_dir}"""
