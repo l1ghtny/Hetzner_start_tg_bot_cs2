5783509eaa8f25b6dc3c57a311ea432185444889
lightny <ankoshenkov@gmail.com>
2024-02-02 19:57:28 +0300

    trying to fix docker .env file

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3
 
-WORKDIR /home/tg-server-bot/
+WORKDIR /home/tgserver/
 RUN mkdir logs
 
 COPY requirements.txt ./
--- src/logs_setup/vars.py
+++ src/logs_setup/vars.py
@@ -1,21 +1,15 @@
 import os
 import platform
-from pprint import pprint
-
 from dotenv import load_dotenv
 
 load_dotenv()
-try:
-    windows_dir = os.getenv("path_main")
-except:
-    print('not successful..')
+windows_dir = os.getenv("path_main")
 
 target_dir = "logs"
 if platform.system() == "Linux":
-    root_dir = "home/tg-server-bot"
+    root_dir = "home/tgserver"
 else:
     disk = "F:\\"
     dir = f"""{windows_dir}"""
     root_dir = os.path.join(disk, dir)
 path = os.path.join(root_dir, target_dir)
-print(path)
